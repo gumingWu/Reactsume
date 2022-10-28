a666ba0033d0fbea271a1bc2589f75252aefa7ed
gumingWu <15626185054@163.com>
2022-10-28 17:38:02 +0800

    添加github图标

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,18 @@
 export function Header() {
   return (
-    <div className='h-12 bg-#1e1e1e flex items-center px-8'>
-      <div className="logo mr-2"></div>
-      <div className='headerTitle'>Reactsume</div>
+    <div className='h-12 bg-#1e1e1e flex items-center justify-between px-8'>
+      <div className="flex items-center">
+        <div className="logo mr-2"></div>
+        <div className='headerTitle'>Reactsume</div>
+      </div>
+      <div>
+        <a href="https://juejin.cn/user/1469381099657902" target="_blank">
+          <div className="i-reactsume-juejin headerIcon"></div>
+        </a>
+        <a href="https://github.com/gumingWu/Reactsume" target="_blank">
+          <div className="i-carbon-logo-github headerIcon"></div>
+        </a>
+      </div>
     </div>
   )
 }
